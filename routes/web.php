4986faf118e98dd904b49bc1de822ac63a6bc690
ibrahim <ibrahim.cheryoukh@student.ehb.be>
2026-05-19 17:16:55 +0200

    feat: routes voor nieuws edit en update toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsItemController;
+use App\Http\Controllers\Userzone\ProfileController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,21 +14,31 @@ Route::get('/dashboard', function () {
     return view('userzone.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/profile/{user}', [App\Http\Controllers\Userzone\ProfileController::class, 'show'])->name('profile.show');
+
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 Route::get('/nieuws', [NewsItemController::class, 'index'])->name('nieuws.index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/nieuws/create', [NewsItemController::class, 'create'])->name('nieuws.create');
     Route::post('/nieuws', [NewsItemController::class, 'store'])->name('nieuws.store');
+    
+    Route::get('/nieuws/{newsItem}/edit', [NewsItemController::class, 'edit'])->name('nieuws.edit');
+    Route::patch('/nieuws/{newsItem}', [NewsItemController::class, 'update'])->name('nieuws.update');
 });
 
 Route::get('/nieuws/{newsItem}', [NewsItemController::class, 'show'])->name('nieuws.show');
+
+
+
+Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
+
 
 require __DIR__.'/auth.php';
