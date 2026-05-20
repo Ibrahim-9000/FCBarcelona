6b115b80da63e60635780cb6b99f022896bdb394
ibrahim <ibrahim.cheryoukh@student.ehb.be>
2026-05-20 21:29:04 +0200

    feat: Admin UserController met index, toggleAdmin en destroy

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function toggleAdmin(User $user)
+    {
+        $user->update(['is_admin' => !$user->is_admin]);
+        return redirect()->route('admin.users.index')->with('success', 'Admin status aangepast!');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('admin.users.index')->with('success', 'Gebruiker verwijderd!');
+    }
+}
