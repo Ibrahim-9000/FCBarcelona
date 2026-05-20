63df40ed97e88e200c5c40436c0112ab15165741
ibrahim <ibrahim.cheryoukh@student.ehb.be>
2026-05-20 16:07:12 +0200

    feat: FaqItemController store en destroy methode

--- app/Http/Controllers/FaqItemController.php
+++ app/Http/Controllers/FaqItemController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FaqCategory;
+use App\Models\FaqItem;
 use Illuminate\Http\Request;
 
 class FaqItemController extends Controller
 {
-    //
+    public function store(Request $request, FaqCategory $faqCategory)
+    {
+        $validated = $request->validate([
+            'question' => 'required|string|max:255',
+            'answer' => 'required|string',
+        ]);
+
+        $validated['faq_category_id'] = $faqCategory->id;
+
+        FaqItem::create($validated);
+
+        return redirect()->route('faq.index')->with('success', 'Vraag aangemaakt!');
+    }
+
+    public function destroy(FaqItem $faqItem)
+    {
+        $faqItem->delete();
+
+        return redirect()->route('faq.index')->with('success', 'Vraag verwijderd!');
+    }
 }
