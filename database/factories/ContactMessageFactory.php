0946dca1ae9eadfed1e298c73c9b3ad827734c79
ibrahim <ibrahim.cheryoukh@student.ehb.be>
2026-05-24 22:59:13 +0200

    feat: ContactMessageFactory aangemaakt met fake data

---
+++ database/factories/ContactMessageFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactMessageFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'message' => fake()->paragraph(3),
+            'is_read' => fake()->boolean(),
+        ];
+    }
+}
